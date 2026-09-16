a9e3c3594c0d6a9e85bb6d99ea3ef447f1ff6769
Knalb <minhao.rat@gmail.com>
2026-09-16 16:45:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+    const heroImage = document.querySelector(".hero-image");
+
     const images = [
+        "home.jpg",
         "automobile.jpg",
         "landscape.jpg",
         "street.jpg"
@@ -8,17 +11,27 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const preloadImages = images.map(src => {
         return new Promise(resolve => {
+
             const img = new Image();
 
             img.onload = resolve;
             img.onerror = resolve;
 
             img.src = src;
+
         });
     });
 
     Promise.all(preloadImages).then(() => {
+
+        // 第一屏显示
+        if (heroImage) {
+            heroImage.classList.add("loaded");
+        }
+
+        // 三张作品封面同时显示
         document.body.classList.add("images-loaded");
+
     });
 
 });
