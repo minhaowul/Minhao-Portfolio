9704b34bcd92ce2b4efe37c4d26bb2b3ad93b37b
Knalb <minhao.rat@gmail.com>
2026-09-17 11:20:48 +0800

    Update automobile.js

--- automobile.js
+++ automobile.js
@@ -194,10 +194,6 @@ Promise.all(preloadPromises).then(() => {
     preloadLargeImages();
 }, 100);
 
-    setTimeout(() => {
-    alignGalleryBottom();
-}, 150);
-
 });
 
 
@@ -387,25 +383,3 @@ document.addEventListener("keydown", event => {
     }
 
 });
-
-function alignGalleryBottom() {
-    const galleries = [
-        document.querySelector(".automobile-gallery"),
-        document.querySelector(".landscape-gallery"),
-        document.querySelector(".street-gallery")
-    ].filter(Boolean);
-
-    if (galleries.length === 0) return;
-
-    galleries.forEach(gallery => {
-        gallery.style.minHeight = "";
-    });
-
-    const maxHeight = Math.max(
-        ...galleries.map(gallery => gallery.offsetHeight)
-    );
-
-    galleries.forEach(gallery => {
-        gallery.style.minHeight = `${maxHeight}px`;
-    });
-}
