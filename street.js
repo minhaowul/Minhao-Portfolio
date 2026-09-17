9f65a83a9370bb35cf00ea2200765d76e2c258cd
Knalb <minhao.rat@gmail.com>
2026-09-17 11:17:29 +0800

    Update street.js

--- street.js
+++ street.js
@@ -124,6 +124,10 @@ Promise.all(preloadPromises).then(() => {
         preloadLargeImages();
     }, 100);
 
+    setTimeout(() => {
+    alignGalleryBottom();
+}, 150);
+
 });
 
 
@@ -337,3 +341,25 @@ lightbox.addEventListener("click", event => {
     }
 
 });
+
+function alignGalleryBottom() {
+    const galleries = [
+        document.querySelector(".automobile-gallery"),
+        document.querySelector(".landscape-gallery"),
+        document.querySelector(".street-gallery")
+    ].filter(Boolean);
+
+    if (galleries.length === 0) return;
+
+    galleries.forEach(gallery => {
+        gallery.style.minHeight = "";
+    });
+
+    const maxHeight = Math.max(
+        ...galleries.map(gallery => gallery.offsetHeight)
+    );
+
+    galleries.forEach(gallery => {
+        gallery.style.minHeight = `${maxHeight}px`;
+    });
+}
